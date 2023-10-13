e3083afdff89cf23cf6cdf9f03456296c7b4df98
satori8 <s.tocheny@gmail.com>
2023-10-13 19:14:12 +0300

    hui

--- main.py
+++ main.py
@@ -1,57 +1,6 @@
 import random
-
+from ids import Users, magicball, Rooms
 from hata import Client, wait_for_interruption, IntentFlag, create_partial_channel_from_id
-
-users = {
-    "kuzya": '334727140241047553',
-    "bodya": '692105234759155744',
-    'inna': '705803066955399209',
-    "boris": '582213686094200870',
-    "taras": '542056426886135838',
-    'strelya': '642786993994072074',
-    'satori': '717701525778071572',
-    "relock": "582247565987414047",
-    "pasha": "721356775697154050"
-}
-
-dota_list = [
-    users['bodya'],
-    users['inna'],
-    users['boris'],
-    users['taras'],
-    users['satori'],
-    users['pasha'],
-    users['relock']
-]
-
-civa_list = [
-    users['bodya'],
-    users['inna'],
-    users['taras']
-]
-
-magicball = [
-    "Так.",
-    "Ні.",
-    "Спробуй ще раз пізніше.",
-    "Неможливо передбачити зараз.",
-    "Питання неоднозначне.",
-    "Все вказує на позитивну відповідь.",
-    "Все вказує на негативну відповідь.",
-    "Шанси на успіх великі.",
-    "Не спирайся на це.",
-    "Можливо, але не впевнено.",
-    "Можливо, з певними сумнівами.",
-    "Спробуй ще раз.",
-    "Перспективи хороші.",
-    "Знаки вказують на позитивний результат.",
-    "Знаки вказують на негативний результат.",
-    "Зосередься і спитай знову.",
-    "Запитай пізніше.",
-    "Важко сказати.",
-    "Краще не розповідати.",
-    "Сконцентруйся і питай знову."
-]
 
 
 def randquote():
@@ -63,12 +12,7 @@ def randquote():
 with open('token', 'r') as f:
     token = f.readline()
 
-Nue = Client(token,
-             intents=IntentFlag().update_by_keys(
-                 guild_users=False,
-                 guild_presences=False,
-
-             ))
+Nue = Client(token, intents=IntentFlag().update_by_keys( guild_users=False, guild_presences=False, ))
 
 
 @Nue.events
@@ -87,7 +31,6 @@ async def message_create(client, message):
         await client.message_create(chan, txt)
         return
 
-    # ohuyenno vazhnij koment
     if client.mentioned_in(message) or (message.content and message.content.startswith("/")):
         if '???' in message.content:
             await client.message_create(message, random.choice(magicball))
@@ -102,13 +45,13 @@ async def message_create(client, message):
             await client.message_create(message, random.choice(arr))
         elif "dota" in message.content:
             msg = ''
-            for user in dota_list:
+            for user in Users.dota_list:
                 msg += f"<@{user}> "
             msg += "Cybersport time: Dota!"
             await client.message_create(message.channel, msg)
         elif "civa" in message.content:
             msg = ''
-            for user in civa_list:
+            for user in Users.civa_list:
                 msg += f"<@{user}> "
             msg += "Cybersport time: Civa!"
             await client.message_create(message.channel, msg)
---
+++ worleparser.py
@@ -0,0 +1,5 @@
+from ids import Users, Rooms
+
+class Wordle:
+    def __init__(self):
+        i = 0
